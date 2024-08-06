5d94f2880f0b817dd4cbd6c24b656fd383218b9a
ellenlimaraquel <134058983+ellenlimaraquel@users.noreply.github.com>
2024-08-06 10:31:40 -0300

    Create pergunta.js

---
+++ pergunta.js
@@ -0,0 +1,106 @@
+ export const perguntas = [
+    {
+        enunciado: "Frio é a presença de baixa temperatura, principalmente na atmosfera, que expressa ou parece possuir uma temperatura mais baixa do que a corporal. O frio interfere no seu cotidiano?",
+        alternativas: [
+            {
+                texto:  "Sim,pois tenho uma rotina para seguir.",
+                afirmacao:[
+                    "O frio nos ajuda a queimar mais calorias, e melhora o funcionamento cerebral. ", 
+                    "O frio pode sim trazer benefícios para a nossa vida cotidiana."
+                ]
+            },
+            {
+                texto: "Não,pois fico no conforto da minha casa e não faço nada",
+                afirmacao: [
+                "O frio deixa nossa pele ressecada, baixa imunidade e também favorece a atividade de vírus respiratórios.",
+                "Porém é necessário um cuidado a mais com a saude, pois com as baixas temperaturas nosso imunológico fica fragilizado."
+                ]
+            }
+           
+        ]
+    },
+    {
+        enunciado: "Para você apartir de quantos graus é considerada uma temperatura baixa? ",
+        alternativas: [
+            {
+                texto: "A baixo de 22° graus.",
+                afirmacao:[
+                    "É suficiente para manter o ambiente confortável sem sobrecarregar o sistema de aquecimento. ",
+                    "As temperaturas intermediarias tem sido as mais queridas pelo povo curitibano."
+                ]
+            },
+            {
+                texto: "A baixo de 15° graus",
+                afirmacao:[
+                    " O clima já está mais fresco, pronto para tirar os casacos do armário.",
+                    "Com o clima mais gelado, começa os estresse e sufocamentos ao termos que vestir varias blusas e casacos grossos."
+                ]
+            }
+           
+        ]
+    },
+    {
+        enunciado: "Quais são suas estações preferidas do ano?",
+        alternativas: [
+            {
+                texto:"Primavera, verão.",
+                afirmacao:[
+
+                 "São as estações do ano que marcam o tempo de plantar e de colher, época de flores, favorável para atividades físicas ao ar livre.",
+                 "É fato que uma paisagem com flores, um lindo sol, passáros cantando e uma caminhada ao ar livre sem se preocupar com casacos e guarda-chuvas, aquece o coração de qualquer um."
+                ]
+            },
+            {
+                texto:  "Outono, inverno.",
+                afirmacao:[
+                    "Clima mais ameno e menor exposição á luz possibilitam descansos mais agradáveis e revigorantes.",
+                    "Com o frio aquele cochilinho depois do almoço para descansar, se torna bem mais aconchegante."
+                ]
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Qual tipo de clima te agrada mais?",
+        alternativas: [
+            {
+                texto:" O clima quente.",
+                afirmacao:[
+                    " Gosto de roupas leves, de tomar coisas geladas, de tomar um sol na praia...",
+                    "A sensação de sentir a brisa do mar no final do ano, pegando uma onda, curtindo um som e tomando um sorvete é maravilhosa."
+                ]
+            },
+            {
+                texto: "O clima frio. ",
+                afirmacao:[
+                    "Prefiro usar muitas roupas e não suar a todo momento por conta do calor, gosto de ficar debaixo das cobertas e tomar um chocolate quente.",
+                    "Tomar um café quente, comer uma sopa e ficar enrolado no cobertor traz uma sensação de conforto muito boa."
+                ]
+            }
+          
+        ]
+    },
+    {
+        enunciado: " Se você não conhece a neve, gostaria de conhece-lá? ",
+        alternativas: [
+            {
+                texto: "Sim, quero",
+                afirmacao:[
+
+                 "Apesar do frio ser extremo, é algo lindo que todos deviamos ter a oportunidade de apreciar ",
+                 "Olhar o frio apenas da janela de casa sem ter nada pra fazer é deveras entediante, mas imagina o qual adimirável seria viajar para um país distante, para ver a neve pela primeira vez e ter a oportunidade até de esquiar."
+                ]
+            },
+            {
+                texto: "Não",
+                afirmacao:[
+                    " Eu não quero sair do conforto da minha casa quentinha, para ver um monte de gelo e congelar de tanto frio.",
+                    "Porém para ver neve devemos lembrar que são temperaturas bem abaixo de zero, o que foge do nosso inverno brasileiro. "
+                ]
+            }
+           
+        ]
+    },
+    
+
+];
